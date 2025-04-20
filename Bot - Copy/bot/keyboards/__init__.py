357bf3760474c70a4c0e29b5f6cd1d67271dbe7e
Gleb <137695628+nezhdanov2000@users.noreply.github.com>
2025-04-20 19:18:43 +0700

    Add files via upload

---
+++ Bot - Copy/bot/keyboards/__init__.py
@@ -0,0 +1,8 @@
+from .main_menu import get_main_menu
+from .booking_keyboards import get_days_keyboard, get_time_keyboard
+
+__all__ = [
+    'get_main_menu',
+    'get_days_keyboard',
+    'get_time_keyboard'
+]
